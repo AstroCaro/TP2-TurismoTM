d4e831121dc2dcb3ca9803a3bd226dfff889ae1c
AstroCaro <carolina@debian>
2021-10-21 20:51:10 -0300

    Se carga repositorio

--- src/main/java/paqueteTurismoTM/Promocion.java
+++ src/main/java/paqueteTurismoTM/Promocion.java
@@ -3,15 +3,13 @@ package paqueteTurismoTM;
 import java.util.ArrayList;
 
 public abstract class Promocion extends Oferta {
-	protected int id_promocion;
 	public ArrayList<String> atracciones;
 	protected int costo;
 	protected double tiempoTotal;
 	protected int cuposDisponibles;
 
-	public Promocion(int id_promocion, String nombre, TipoAtraccionENUM tipoAtraccion, ArrayList<String> atracciones) {
+	public Promocion(String nombre, String tipoAtraccion, ArrayList<String> atracciones) {
 		super(nombre, tipoAtraccion);
-		this.id_promocion=id_promocion;
 		this.atracciones = new ArrayList<String>(atracciones);
 	}
 
@@ -22,7 +20,7 @@ public abstract class Promocion extends Oferta {
 	public double getTiempo() {
 		tiempoTotal = 0;
 		for (String a : atracciones) {
-			for (Oferta b : new LectorDeFicheros().cargarAtracciones()) {
+			for (Oferta b : TurismoTM.atracciones) {
 				if (a.equals(b.nombre)) {
 					tiempoTotal += b.tiempo;
 				}
@@ -46,7 +44,7 @@ public abstract class Promocion extends Oferta {
 	public int getCuposDisponibles() {
 		int cupoDisponible = 9999;
 		for (String a : atracciones) {
-			for (Oferta b : TurismoTM.ofertas) {
+			for (Oferta b : TurismoTM.atracciones) {
 				if (a.equals(b.nombre)) {
 					if (b.getCuposDisponibles() < cupoDisponible) {
 						cupoDisponible = b.getCuposDisponibles();
